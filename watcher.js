e308626b99480a563e27bc9546b6340762524429
Albert Chen <albert.cht@gmail.com>
2018-06-03 13:50:31 +0800

    require isRunning

--- watcher.js
+++ watcher.js
@@ -1,10 +1,11 @@
 let fs = require('fs')
 let chokidar = require('chokidar')
 let colors = require('colors/safe')
+let isRunning = require('is-running')
 
 // Define our watching parameters
 let basePath = process.cwd()
-let pidPath = basePath + '/storage/logs/swoole_http.pid'
+let pidPath = process.cwd() + '/storage/logs/swoole_http.pid'
 let pid = fs.readFileSync(pidPath, 'utf8')
 let ready = false
 
@@ -17,7 +18,7 @@ let logger = (color, message, level = 'log', skipSignal = false) => {
 }
 
 let sendSignal = () => {
-  if (!require('is-running')(pid)) {
+  if (!isRunning(pid)) {
     ready = false
     logger('red', `PID ${pid} is not alive. Close watcher process.`, 'error')
     process.exit()
@@ -27,7 +28,7 @@ let sendSignal = () => {
   logger('green', `Reloading process PID ${pid}...`, 'log', true)
 }
 
-if (!require('is-running')(pid)) {
+if (!isRunning(pid)) {
   logger('red', `PID ${pid} is not alive.`, 'error')
   return
 } else {
